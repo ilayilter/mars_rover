a82278cf77fbb18a5435916e9a8b863dc1387312
Ilay.Ilter <ilay.ilter@logo.com.tr>
2022-03-03 10:35:13 +0300

    kbox v1

---
+++ kbox/useractivity/src/main/java/com/kbox/useractivity/UserActivityApplication.java
@@ -0,0 +1,12 @@
+package com.kbox.useractivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityApplication.class, args);
+	}
+}
